0076eb152b83ced55f5d35d25e05b6e719aecd0b
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-17 02:02:19 +0300

    Add initial TypesAndVariables project
    Created a new C# console application demonstrating basic types and variables, including enums and various primitive types. Added solution and project files targeting .NET 9.0.

---
+++ TypesAndVariables/TypesAndVariables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace TypesAndVariables
+{
+    class program
+    {
+        static void Main()
+        {
+            // Console.WriteLine("Hello World");
+            double number5 = 10.5;
+            decimal number6 = 10.4m;//10.4M
+            char character = 'a';
+            int number1 = 10;
+            long number2 = 2147483649;
+            short number3 = 32767;
+            byte number4 = 255;
+            bool is_active = true;
+            var number7 = 10;
+            var character2 = 'b';
+            Console.WriteLine(number1);
+            Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number2 is {0}",number2);
+            Console.WriteLine("Number3 is {0}",number3);
+            Console.WriteLine("Number4 is {0}",number4);
+            Console.WriteLine("Number5 is {0}",number5);
+            Console.WriteLine("Number7 is {0}",number7);
+            Console.WriteLine("Character is {0}",character);
+            Console.WriteLine("Character is {0}",character);
+            Console.WriteLine("Character2 is {0}",(int)character2);
+            Console.WriteLine(Days.Friday);
+            Console.WriteLine((int)Days.Friday);
+
+
+        }
+    }
+    enum Days
+    {
+        Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+    }
+}
