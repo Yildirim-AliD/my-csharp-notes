5fc44b003a77b5d73f4bfbf8286fd2f69767c3ed
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-27 17:59:37 +0300

    Add 04_Arrays project with array examples
    Introduced a new project '04_Arrays' demonstrating array initialization and iteration in C#. Updated the solution file to include the new project.

---
+++ CsharpNotes/04_Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[4];
+            students[0] = "Ali";
+            students[1] = "Yıldırım";
+            students[2] = "Emir";
+            students[3] = "Erol";
+
+            string[] students2 = new [] { "Ali", "Yıldırım", "Emir", "Erol" };
+            string[] students3 = { "Ali", "Yıldırım", "Emir", "Erol" };
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+            string[,] regions = new string[7,3]
+            {
+                { "Adana", "Mersin", "Hatay" },
+                { "İstanbul", "Kocaeli", "Sakarya" },
+                { "Ankara", "Konya", "Eskişehir" },
+                { "Bursa", "Balıkesir", "Çanakkale" },
+                { "Trabzon", "Rize", "Artvin" },
+                { "Gaziantep", "Şanlıurfa", "Diyarbakır" },
+                { "İzmir", "Muğla", "Aydın"}
+            };
+            Console.WriteLine("*****************");
+            for (int i = 0; i  <= regions.GetUpperBound(0);i++)
+            {
+                for(int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("*****************");
+            }
+
+        }
+    }
+}
