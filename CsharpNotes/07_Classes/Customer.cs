3a7ffc4c8805ac235b86675c270019d02bf3e676
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-27 23:34:55 +0300

    Add 01_RecapDemo and 07_Classes projects
    Introduced two new projects: 01_RecapDemo, a Windows Forms application demonstrating dynamic button grid creation, and 07_Classes, a console app illustrating basic class usage with Customer, CustomManager, and ProductManager. Updated the solution file to include both projects.

---
+++ CsharpNotes/07_Classes/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Classes
+{
+    internal class Customer
+    {
+        // Properties of the Customer class
+        public int Id { get; set; }
+        private string _firstName; // Backing field for FirstName
+        public string FirstName { get
+            {
+                return "Mr." + _firstName;
+            }
+            set { _firstName = value; } }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ CsharpNotes/07_Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomManager customManager = new CustomManager();
+            customManager.Add();
+            customManager.Update();
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Ali";
+            customer.LastName = "Yıldırım";
+            customer.City = "Diyarbakır";
+
+            Console.WriteLine("Customer 1: " + customer.FirstName + " " + customer.LastName + ", City: " + customer.City);
+        }
+    }
+}
