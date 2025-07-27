2a59bdc17e1c820828e20f2a5b5a49922ee4e7ef
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-27 21:22:38 +0300

    Add 05_Loops and 06_Strings projects to solution
    Added new projects 05_Loops and 06_Strings with their respective source files, configuration, and assembly info. Updated the solution file to include both projects and their build configurations.

---
+++ CsharpNotes/05_Loops/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace _05_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ForLoops();
+            //WhileLoops();
+            //DoWhileLoops();
+            //NestedLoops();
+            //DoNestedLoops();
+            //forEachLoop();
+            if(IsPrimeNumber(29))
+            {
+                Console.WriteLine("29 is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("29 is not a prime number.");
+            }
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for(int i = 2; i<number/2; i++)
+            {
+                if(number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private static void ForLoops()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void WhileLoops()
+        {
+            int i = 1;
+            while (i <= 20)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        private static void DoWhileLoops()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 20);
+        }
+        private static void NestedLoops()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.WriteLine($"i: {i}, j: {j}");
+                }
+            }
+        }
+        private static void DoNestedLoops()
+        {
+            int i = 1;
+            do
+            {
+                int j = 1;
+                do
+                {
+                    Console.WriteLine($"i: {i}, j: {j}");
+                    j++;
+                } while (j <= 5);
+                i++;
+            } while (i <= 5);
+        }
+        private static void forEachLoop()
+        {
+            string[] students = { "Ali", "Yıldırım", "Emir", "Erol" };
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+    }
+}
---
+++ CsharpNotes/06_Strings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro();
+            string sentence = "My name is Ali yıldırım";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            bool result3 = sentence.EndsWith("Ali");
+            bool result4 = sentence.StartsWith("My");
+
+            var result5 = sentence.IndexOf("name");
+            var result6 = sentence.IndexOf("Name");
+            var result7 = sentence.LastIndexOf("i");
+            var result8 = sentence.Insert(0, "Hello,");
+            var result9 = sentence.Substring(3,4);
+            var result10 = sentence.ToLower();
+            var result11 = sentence.ToUpper();
+            var result12 = sentence.Replace(" ", "-");
+            var result13 = sentence.Remove(2);
+            Console.WriteLine(result);
+        }
+
+        private static void Intro()
+        {
+            string city = "İzmir";
+            //Console.WriteLine(city[0]);
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "İstanbul";
+            //string result = city + " " + city2;
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+        }
+    }
+}
