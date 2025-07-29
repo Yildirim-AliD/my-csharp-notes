fd0c9e7eaa6664d328f975e986b283a431338f71
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-29 23:22:27 +0300

    Add projects for virtual methods and abstract classes
    Added new C# projects '10_VirtualMethod' and '11_AbstractClasses' with example code demonstrating virtual methods and abstract classes. Updated the solution file to include these new projects.

---
+++ CsharpNotes/11_AbstractClasses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database  = new Oracle();
+            database.Add();
+            database.Delete();
+
+            Database database2 = new SQLServer();
+            database2.Add();
+            database2.Delete();
+        }
+    }
+
+    abstract class Database
+    {
+        public  void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public abstract void Delete();
+    }
+
+    class SQLServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql Code");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle Code");
+        }
+    }
+}
