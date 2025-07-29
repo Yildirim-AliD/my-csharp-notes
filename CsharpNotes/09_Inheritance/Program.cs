4bb418860ffd6fd192ad9abfbbb913d675c3b3ca
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-29 23:03:15 +0300

    Add 09_Inheritance project with inheritance examples
    Introduced a new C# project '09_Inheritance' demonstrating basic inheritance concepts. Added Program.cs with Person, Customer, and Student classes, as well as supporting project and configuration files. Updated the solution file to include the new project.

---
+++ CsharpNotes/09_Inheritance/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _09_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] persons = new Person[]
+            {
+               new Customer
+               {
+                     Id = 1,
+                     FirstName = "Ali",
+                     LastName = "Yıldırım",
+                     City = "İzmir"
+               },new Student
+               {
+                        Id = 2,
+                        FirstName = "Yıldırım",
+                        LastName = "Yılmaz",
+                        Department = "Computer Science"
+               },new Person
+               {
+                   FirstName = "Mehmet",
+               }
+
+            };
+
+            foreach(var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Person2
+    {
+
+    }
+
+    interface IPerson3
+    {
+
+    }
+
+    class Customer : Person, IPerson3
+    {
+        public string City { get; set; }
+    }
+    
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
