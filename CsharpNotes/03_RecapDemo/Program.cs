bace35f89eab16961b1553edbacc072bcfc23e24
Yildirim-AliD <yldrmalid@gmail.com>
2025-07-30 00:24:56 +0300

    Add 03_RecapDemo project with logging demo
    Introduced a new C# console project '03_RecapDemo' demonstrating basic dependency injection with ILogger, Logger, and FileLogger implementations. Updated the solution file to include the new project.

---
+++ CsharpNotes/03_RecapDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_RecapDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new Logger(); // or new FileLogger();
+            customerManager.Add();
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer added.");
+        }
+    }
+
+    class Logger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Database!");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to File!");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+}
