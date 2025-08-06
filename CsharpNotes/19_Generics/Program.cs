3589e4537084efd06af52aa261fd93dd8264334b
Yildirim-AliD <yldrmalid@gmail.com>
2025-08-06 15:45:52 +0300

    Add 19_Generics project with generics demo
    Introduced a new project '19_Generics' demonstrating the use of generics in C#. Includes example classes, interfaces, and repository patterns for Product and Customer entities. Updated the solution file to include the new project.

---
+++ CsharpNotes/19_Generics/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("İzmir", "Ankara", "Adana");
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            List<Customer> result2 = utilities.BuildList<Customer>(new Customer { FirstName = "Ali"}, new Customer { FirstName = "Yıldırım" });
+
+            foreach(var customer in result2)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+        }
+
+    }
+
+   class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+
+    class Product:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    interface IProductDal:IRepository<Product>
+    {
+        
+    }
+    
+
+
+    class Customer:IEntity
+    {
+        public string FirstName { get; set; }
+    }
+
+    interface ICustomerDal:IRepository<Customer>
+    {
+        void Custom();
+
+    }
+    //interface IStudentDal:IRepository<string>
+    //{
+
+    //}
+    //interface IStudentDal : IRepository<CustomerDal>
+    //{
+
+    //}
+    //interface IStudentDal:IRepository<İNT>
+    //{
+
+    //}
+
+    class Student :IEntity
+    {
+
+    }
+
+    interface IEntity
+    {
+
+    }
+
+
+    interface IRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+
+
+    class ProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Custom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
