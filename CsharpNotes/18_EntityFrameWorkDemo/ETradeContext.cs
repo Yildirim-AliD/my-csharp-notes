53f01b587eb3db5c21fa0a51f817d085091eb196
Yildirim-AliD <yldrmalid@gmail.com>
2025-08-06 13:37:35 +0300

    Add Entity Framework WinForms demo project
    Introduced a new project '18_EntityFrameWorkDemo' demonstrating basic CRUD operations using Entity Framework in a WinForms application. Includes Product entity, data access layer, context, and UI for managing products. Updated the solution file to include the new project.

---
+++ CsharpNotes/18_EntityFrameWorkDemo/ETradeContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace _18_EntityFrameWorkDemo
+{
+    internal class ETradeContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CsharpNotes/18_EntityFrameWorkDemo/ProductDal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_EntityFrameWorkDemo
+{
+    public class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void Add(Product product)
+        {
+            using (ETradeContext ctx = new ETradeContext())
+            {
+                //ctx.Products.Add(product);
+                var entity = ctx.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Added;
+                ctx.SaveChanges();
+            }
+        }
+
+
+        public void Update(Product product)
+        {
+            using (ETradeContext ctx = new ETradeContext())
+            {
+                var entity = ctx.Entry(product);
+                entity.State =System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            
+            using (ETradeContext ctx = new ETradeContext())
+            {
+                var entity = ctx.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
