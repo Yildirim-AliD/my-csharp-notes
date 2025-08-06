1142ac1f60d25836bd493c522e0523f583d8d899
Yildirim-AliD <yldrmalid@gmail.com>
2025-08-07 01:24:11 +0300

    Add 23_Events project demonstrating C# events
    Introduced a new project '23_Events' to the solution, including Product and Program classes to demonstrate event handling in C#. The Product class raises an event when stock is low, and Program subscribes to this event for GSM products. Project files and configuration have been added.

---
+++ CsharpNotes/23_Events/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(50);
+            product.Name = "Laptop";
+
+            Product gsm = new Product(50);
+            gsm.Name = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+
+            for (int i = 0; i < 10; i++)
+            {
+                product.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("Stock is low for GSM. Please restock.");
+        }
+    }
+}
